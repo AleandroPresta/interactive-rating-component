85bbf01665e8ff7fd2e3ed8b5ea5735db547055f
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-08 13:16:32 +0200

    Added eventlistener that changes all the circles to white

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const btnSubmit = document.getElementById('btnSubmit');
 const ratingCard = document.querySelector('.rating-card');
+const white = 'hsl(0, 0%, 100%)';
+const ratingCircles = document.querySelectorAll('.circle');
 
-
-
-btnSubmit.addEventListener("click", submitClick);
+btnSubmit.addEventListener('click', submitClick);
 
 function submitClick(){
 
     window.location.href = 'thank-you.html';
+
+}
+
+ratingCircles.forEach(
+    (circle) => {
+        circle.addEventListener('click', () => selectRating(circle));
+    }
+);
+
+function selectRating(circle){
+
+    circle.style.backgroundColor = white;
 
 }
