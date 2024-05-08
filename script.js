c7920daf50a56679930340034fa964f792cf0eac
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-05-08 14:35:56 +0200

    Fixing the hover effect on the circles when a circle is clicked

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const btnSubmit = document.getElementById('btnSubmit');
 const ratingCard = document.querySelector('.rating-card');
 const ratingCircles = document.querySelectorAll('.circle');
 
+console.log(ratingText);
+
 btnSubmit.addEventListener('click', submitClick);
 
 function submitClick(){
@@ -19,7 +21,19 @@ ratingCircles.forEach(
 const clickBackgroundColor = 'hsl(217, 12%, 63%)';
 const clickTextColor = 'hsl(0, 0%, 100%)';
 
+const defaultBackgroundColor = 'hsl(216, 12%, 54%, 0.2)';
+const defaultTextColor = 'hsl(217, 12%, 63%)';
+
 function selectRating(circle){
+
+    // Reset color of all the circles
+    
+    ratingCircles.forEach(
+        (element) => {
+            element.style.backgroundColor = defaultBackgroundColor;
+            element.style.color = defaultTextColor;
+        }
+    );
 
     circle.style.backgroundColor = clickBackgroundColor;
     circle.style.color = clickTextColor;
